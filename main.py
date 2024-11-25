be1757133218203331d9fc78489d7e7de55c895d
RafBel94 <beltran.caraf23@cadiz.salesianos.edu>
2024-11-25 14:11:27 +0100

    Auto stash before merge of "main" and "origin/main"

--- main.py
+++ main.py
@@ -3,7 +3,25 @@ import time
 import logwriting
 import databasewriting
 
-collection = connection.getDatabaseWithCollection()
+def obtener_temperatura_cpu(url="http://localhost:8085/data.json"):
+    try:
+        # Solicitar datos del servidor de OpenHardwareMonitor
+        response = requests.get(url)
+        data = response.json()
+        
+        # Recorrer los sensores para encontrar la temperatura de la CPU
+        for hardware in data['Children']:
+            for sensor in hardware['Children']:
+                if sensor['Text'] == 'Intel Core i7-9850H': # Replace with your CPU model
+                    for core in sensor['Children']:
+                        for temp in core['Children']:
+                            if temp['Text'] == 'Temperatures':
+                                return core['Value']
+                            
+        else:
+            print("No se encontraron sensores de temperatura para la CPU.")
+    except Exception as e:
+        print(f"Error al obtener los datos: {e}")
 
 while True:
     logwriting.write_log()
