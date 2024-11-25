2658588277abc0b70c73323da9245aa55f57e969
RafBel94 <beltran.caraf23@cadiz.salesianos.edu>
2024-11-25 17:13:47 +0100

    Implemented CPU Temperature reading ( OpenHardwareMonitor software and server must be up )

--- getinfo.py
+++ getinfo.py
@@ -1,6 +1,8 @@
 import socket
 import psutil
 import pynvml
+import requests
+
 def get_ip():
     hostname = socket.gethostname()
     return socket.gethostbyname(hostname)
@@ -22,6 +24,27 @@ def get_gpu_temp():
     handle = pynvml.nvmlDeviceGetHandleByIndex(0)
     info = pynvml.nvmlDeviceGetTemperature(handle, 0)
     return info
+
+def get_cpu_temp(url="http://localhost:8085/data.json"):
+    try:
+        # Get JSON response from Open Hardware Monitor
+        response = requests.get(url)
+        data = response.json()
+        
+        # Find the CPU temperature sensor and get CPU temperature value
+        for hardware in data['Children']:
+            for sub_hardware in hardware['Children']:
+                if sub_hardware['Text'] == "Intel Core i7-9850H":
+                    for sensor in sub_hardware['Children']:
+                        if sensor['Text'] == "Temperatures":
+                            for temp_sensor in sensor['Children']:
+                                if temp_sensor['Text'] == "CPU Core #1":
+                                    return temp_sensor['Value']
+                                
+        else:
+            print("There are no CPU temperature sensors")
+    except Exception as e:
+        print(f"Error obtaining temperature: {e}")
 
 def get_running_tasks():
     tasks = []
