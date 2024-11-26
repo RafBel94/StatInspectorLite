facdc1708b71be48ec9b000c3f46e124fa50501e
Pablorv004 <pablorv004@gmail.com>
2024-11-26 09:44:46 +0100

    Added condition trigger for activation.

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import time
 import logwriting
 import databasewriting
+import getinfo
 
 if __name__ == "__main__":
-    print("Starting monitoring...")
-    
+    print("Starting monitoring... Press CTRL+C to stop.")
     while True:
-        logwriting.write_log()
-        databasewriting.write_to_database()
+        if(int(str(getinfo.get_cpu_temp()).split(",")[0]) > 45):
+            logwriting.write_log()
+            databasewriting.write_to_database()
         time.sleep(3)
